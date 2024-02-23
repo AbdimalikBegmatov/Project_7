f282380f7a3f77dc7e2a62aea6b68feddd4d348e
AbdimalikBegmatov <abdimalik.b@gmail.com>
2024-02-23 08:59:44 +0600

    fix config file

--- src/main/java/com/example/tour/Configs/WebConfig.java
+++ src/main/java/com/example/tour/Configs/WebConfig.java
@@ -1,2 +1,20 @@
-package com.example.tour.Configs;public class WebConfig {
+package com.example.tour.Configs;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry corsRegistry){
+        corsRegistry
+                .addMapping("/**")
+                .allowedOrigins("https://crazy-zam.github.io/neo-tour")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
